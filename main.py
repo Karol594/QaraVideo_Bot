b5055d41fa908e340f738206326cf0a5cdee109b
Karol594 <karagodlewski@gmail.com>
2025-05-24 00:59:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-python
 import os
 from telegram.ext import Application, CommandHandler
 from dotenv import load_dotenv
 
 load_dotenv()
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text("Сәлем! Мен QaraVideo Bot боламын. Видео жүктеп берейін.😊")
+    await update.message.reply_text("Сәлем! Мен QaraVideo Бот боламын. Видео жүктеп берейін.")
 
 if __name__ == '__main__':
-    PORT = int(os.environ.get("PORT", 8080))
-    app = Application.builder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    port = int(os.environ.get("PORT", 8080))
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
