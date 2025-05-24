6ce09d99c02dfd13f4f8b363cd377a32662655de
Karol594 <karagodlewski@gmail.com>
2025-05-24 10:01:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! I'm QaraVideo Bot. Send me a YouTube link!")
+    await update.message.reply_text("Hello! I'm QaraVideo Bot. Send me a YouTube link to download.")
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
