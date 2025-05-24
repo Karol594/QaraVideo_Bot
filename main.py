fb4da3541d986fcef3647ed375d0af5e9bd4a685
Karol594 <karagodlewski@gmail.com>
2025-05-24 10:29:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I'm QaraVideo Bot. Send me a YouTube link!")
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 8080))  # PORT Fly.io ушын керек
+    PORT = int(os.environ.get("PORT", 8080))
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling(drop_pending_updates=True)  # polling режим
+    app.run_polling(drop_pending_updates=True)
