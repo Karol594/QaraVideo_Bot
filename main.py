e4082363efdefc0f1229502b7a20943be41ae7b9
Karol594 <karagodlewski@gmail.com>
2025-05-24 10:24:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! I'm QaraVideo Bot. Send me a YouTube link to download.")
+    await update.message.reply_text("Hello! I'm QaraVideo Bot. Send me a YouTube link!")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    PORT = int(os.environ.get("PORT", 8080))  # PORT Fly.io ушын керек
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling(drop_pending_updates=True)
+    app.run_polling(drop_pending_updates=True)  # polling режим
